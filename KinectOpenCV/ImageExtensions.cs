51f127c0e068722c1e9b647ba29f29d6de11a667
Thad House <thadhouse1@gmail.com>
2016-09-17 18:44:31 -0700

    Initial commit of OpenCV kinect code

---
+++ KinectOpenCV/ImageExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Microsoft.Kinect;
+
+namespace KinectOpenCV
+{
+    // Code from here
+    //http://www.apress.com/9781430241041
+
+    public static class ImageExtensions
+    {
+
+        public static Mat ToOpenCVMat(this ColorImageFrame image, ref byte[] data, ref Bitmap bitmap, ref Image<Bgr, byte> cvImage)
+        {
+
+            if (image == null || image.PixelDataLength == 0)
+                return null;
+            if (data == null || data.Length < image.PixelDataLength)
+                data = new byte[image.PixelDataLength];
+            image.CopyPixelDataTo(data);
+
+            if (bitmap == null || (bitmap.Width != image.Width && bitmap.Height != image.Height &&
+                bitmap.PixelFormat != PixelFormat.Format32bppRgb))
+            {
+                bitmap = new Bitmap(image.Width, image.Height, PixelFormat.Format32bppRgb);
+            }
+
+
+            var bitmapData = bitmap.LockBits(
+                new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.WriteOnly,
+                bitmap.PixelFormat);
+            Marshal.Copy(data, 0, bitmapData.Scan0, data.Length);
+            bitmap.UnlockBits(bitmapData);
+            if (cvImage == null)
+            {
+                cvImage = new Image<Bgr, byte>(bitmap);
+            }
+            else
+            {
+                cvImage.Bitmap = bitmap;
+            }
+
+            return cvImage.Mat;
+
+            //var img = new Image<Bgr, byte>(bitmap);
+            return null;
+            //return new Image<Bgr, byte>(bitmap).Mat;
+        }
+    }
+}
--- KinectOpenCV/Program.cs
+++ KinectOpenCV/Program.cs
@@ -1,15 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Microsoft.Kinect;
 
 namespace KinectOpenCV
 {
     class Program
     {
+        private static KinectSensor sensor;
+
         static void Main(string[] args)
         {
+            foreach (KinectSensor potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (sensor != null)
+            {
+                sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                sensor.ColorFrameReady += SensorFrameReady;
+
+                try
+                {
+                    sensor.Start();
+                }
+                catch (IOException)
+                {
+                    sensor = null;
+                }
+            }
+
+            if (sensor == null)
+            {
+                Console.WriteLine("The kinect did not start properly. Press any key to exit");
+                Console.ReadKey();
+            }
+            else
+            {
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+
+        private static int count = 0;
+
+        private static byte[] data;
+        private static Bitmap bitmap;
+        private static Mat displayMat = new Mat();
+        private static Image<Bgr, byte> image;
+
+        private static readonly object mutex = new object();
+
+        private static void SensorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+            {
+                Console.WriteLine(count);
+                count++;
+
+                if (frame == null) return;
+
+
+                lock (mutex)
+                {
+
+                    // Convert to OpenCV mat
+                    using (var rawImage = frame.ToOpenCVMat(ref data, ref bitmap, ref image))
+                    {
+                        if (rawImage == null) return;
+                        using (Mat flipped = new Mat())
+                        {
+                            CvInvoke.Flip(rawImage, displayMat, FlipType.Horizontal);
+                        }
+                    }
+
+                    CvInvoke.Imshow("Image", displayMat);
+
+                }
+                CvInvoke.WaitKey(1);
+            }
         }
     }
 }
