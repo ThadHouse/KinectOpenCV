69f419f77026a54e2caa344bf2c73bc6662ce32f
Thad House <thadhouse1@gmail.com>
2016-09-20 10:20:25 -0700

    Some more Changes

---
+++ KinectOpenCV/SearcherBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using NetworkTables;
+using NetworkTables.Tables;
+
+namespace KinectOpenCV
+{
+    struct TrackedRobot
+    {
+        public double Angle { get; }
+        public Point Location { get; }
+    }
+    
+    abstract class SearcherBase
+    {
+        protected ITable table;
+
+        protected SearcherBase(string tablePrefix)
+        {
+            table = NetworkTable.GetTable(tablePrefix);
+        }
+
+        public abstract TrackedRobot FindTarget(Mat rawImage);
+    }
+}
--- KinectOpenCV/TriangleColorSearch.cs
+++ KinectOpenCV/TriangleColorSearch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,12 @@ using Emgu.CV.Structure;
 using Emgu.CV.Util;
 using NetworkTables;
 using NetworkTables.Tables;
+using OpenTK;
 
 namespace KinectOpenCV
 {
-    class TriangleColorSearch
+    class TriangleColorSearch : SearcherBase
     {
-        private ITable table;
 
         static readonly MCvScalar Red = new MCvScalar(0, 0, 255);
         static readonly MCvScalar Green = new MCvScalar(0, 255, 0);
@@ -35,9 +36,8 @@ namespace KinectOpenCV
         private const double defaultMinSize = 100;
 
 
-        public TriangleColorSearch(string tablePrefix)
+        public TriangleColorSearch(string tablePrefix) : base(tablePrefix)
         {
-            table = NetworkTable.GetTable(tablePrefix);
 
             table.SetDefaultNumberArray(HSVLow, defaultLow);
             table.SetDefaultNumberArray(HSVHigh, defaultHigh);
@@ -52,9 +52,14 @@ namespace KinectOpenCV
             table.SetPersistent(maxSize);
         }
 
-        public void TrackImage(Mat rawImage)
+        private Mat HsvDisplay = new Mat();
+        private Mat InRangeDisplay = new Mat();
+        private VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+
+        // Will already be flipped
+        public override TrackedRobot? FindTarget(Mat rawImage)
         {
-            if (rawImage == null) return;
+            if (rawImage == null) return null;
 
             double[] ntLow = table.GetNumberArray(HSVLow, defaultLow);
             double[] ntHigh = table.GetNumberArray(HSVHigh, defaultHigh);
@@ -72,13 +77,109 @@ namespace KinectOpenCV
             arrayHigh.Clear();
             arrayHigh.Push(ntHigh);
 
-            using (var toDisplay = new Mat())
-            
+            using (var temp = new Mat())
+
             {
-                CvInvoke.Flip(rawImage, toDisplay, FlipType.Horizontal);
+                CvInvoke.CvtColor(rawImage, HsvDisplay, ColorConversion.Bgr2Hsv);
+
+                CvInvoke.InRange(HsvDisplay, arrayLow, arrayHigh, InRangeDisplay);
+
+                InRangeDisplay.ConvertTo(temp, DepthType.Cv8U);
+
+                CvInvoke.FindContours(temp, contours, null, RetrType.List, ChainApproxMethod.ChainApproxTc89Kcos);
+
+                VectorOfVectorOfPoint convexHulls = new VectorOfVectorOfPoint(contours.Size);
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    CvInvoke.ConvexHull(contours[i], convexHulls[i]);
+                }
+
+                // Filter convex hulls
+                for (int i = 0; i < convexHulls.Size; i++)
+                {
+                    VectorOfPoint hull = convexHulls[i];
+                    VectorOfPoint polygon = new VectorOfPoint(convexHulls.Size);
+                    CvInvoke.ApproxPolyDP(hull, polygon, 10, true);
+
+                    if (polygon.Size != 3)
+                    {
+                        polygon.Dispose();
+                        continue;
+                    }
+
+                    if (!CvInvoke.IsContourConvex(polygon))
+                    {
+                        polygon.Dispose();
+                        continue;
+                    }
+
+                    double area = CvInvoke.ContourArea(hull);
+
+                    if (area < localMinSize || area > localMaxSize)
+                    {
+                        polygon.Dispose();
+                        continue;
+                    }
+
+                    // Find the shortest line
+
+                    Line shortestLine = new Line(double.MaxValue, 0, 0);
+
+                    for (int k = 0; k < 3; k++)
+                    {
+                        double dx = polygon[k].X - polygon[(k + 1)%3].X;
+                        double dy = polygon[k].Y - polygon[(k + 1)%3].Y;
+
+                        double lineLen = Math.Sqrt(dx*dx + dy*dy);
+
+                        double slopeRad = Math.Atan2(dy, dx);
+
+                        if (lineLen < shortestLine.Length)
+                        {
+                            // Find the opposite point
+                            int oppositeIndex = GetOppositePoint(k, (k + 1)%3);
+
+                            Vector2d pa = new Vector2d(polygon[oppositeIndex].X - polygon[k].X,
+                                polygon[oppositeIndex].Y - polygon[k].Y);
+                            ;
+                            Vector2d d = new Vector2d(dx, dy);
+                            d.Normalize();
+                            double dot = Vector2d.Dot(pa, d);
+
+                            Vector2d multiplied = Vector2d.Multiply(d, dot);
+
+                            Point x = new Point((int) (polygon[k].X + multiplied.X), (int) (polygon[k].Y + multiplied.Y));
 
 
+                            double newDx = polygon[oppositeIndex].X - x.X;
+                            double newDy = polygon[oppositeIndex].Y - x.Y;
+
+
+                            shortestLine = new Line(lineLen, ToDegrees(Math.Atan2(newDy, newDx)), ToDegrees(slopeRad));
+                        }
+                    }
+                }
             }
+        }
+
+        private static int GetOppositePoint(int index1, int index2)
+        {
+            if (index1 == 0 && index2 == 1) return 2;
+            else if (index1 == 0 && index2 == 2) return 1;
+            else if (index1 == 1 && index2 == 2) return 0;
+            else if (index2 == 0 && index1 == 1) return 2;
+            else if (index2 == 0 && index1 == 2) return 1;
+            else return 0;
+        }
+
+        static Point TextPoint = new Point(0, 20);
+        static Point TextPoint2 = new Point(0, 50);
+        static Point TextPoint3 = new Point(0, 80);
+
+        private static double ToDegrees(double radians)
+        {
+            return radians * 180.0 / Math.PI;
         }
     }
 }
